938bc972eeac8152489da85205747feb8d2bed3a
Tony Edgecombe <tonyedgecombe@gmail.com>
2015-11-23 13:38:40 +0000

    Added help message to lpq

--- lpq/LPQProgram.cs
+++ lpq/LPQProgram.cs
@@ -7,6 +7,22 @@ namespace lpq
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Displays the state of a remote lpd queue.");
+                Console.WriteLine();
+                Console.WriteLine("Usage: lpq -Sserver -Pprinter [-l]");
+                Console.WriteLine();
+                Console.WriteLine("Options:");
+                Console.WriteLine("      -S server   Name or ipaddress of the host providing lpd service");
+                Console.WriteLine("      -P printer  Name of the print queue");
+                Console.WriteLine("      -l          verbose output");
+                Console.WriteLine();
+                Console.WriteLine();
+
+                return;
+            }
+
             var job = CommandLineParser.ParseCommandLine(args);
             var lines = LPRClient.QueryPrinter(job);
 
