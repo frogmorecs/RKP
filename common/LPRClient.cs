7f4d69c67f0bcbb7a1e4bd0f736c12c004e7430e
Tony Edgecombe <tonyedgecombe@gmail.com>
2015-11-25 15:34:49 +0000

    Initial lpr code

--- common/LPRClient.cs
+++ common/LPRClient.cs
@@ -1,13 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
 
 namespace common
 {
+    public static class StreamExtensions
+    {
+        public static void Write(this Stream stream, string text)
+        {
+            var data = Encoding.ASCII.GetBytes(text);
+            stream.Write(data, 0, data.Length);
+        }
+    }
+
     public interface ILPRClient
     {
         IEnumerable<string> QueryPrinter(LPQJob lpqJob);
+        void PrintFile(LPRJob job);
     }
 
     public class LPRClient : ILPRClient
@@ -29,6 +41,57 @@ namespace common
                 {
                     yield return streamReader.ReadLine();
                 }
+            }
+        }
+
+        public void PrintFile(LPRJob job)
+        {
+            using (var client = new TcpClient(job.Server, LPRPort))
+            using (var stream = client.GetStream())
+            {
+                stream.Write($"\x02{job.Printer}\n");
+                stream.Flush();
+
+                CheckResult(stream);
+
+                var controlFile = new StringBuilder();
+                controlFile.Append($"H{Environment.MachineName}\n");
+                controlFile.Append($"P{Environment.UserName}\n");
+                controlFile.Append($"odfA001{Environment.MachineName}\n");
+                controlFile.Append($"UdfA001{Environment.MachineName}\n");
+                controlFile.Append($"N{job.Path}\n");
+
+                stream.Write($"\x02{controlFile.Length} cfA001{Environment.MachineName}\n");
+                stream.Flush();
+
+                CheckResult(stream);
+
+                stream.Write(controlFile.ToString());
+                stream.WriteByte(0);
+                stream.Flush();
+
+                CheckResult(stream);
+
+                var fileSize = new FileInfo(job.Path).Length;
+                stream.Write($"\x03{fileSize} dfA001{Environment.MachineName}\n");
+                stream.Flush();
+
+                CheckResult(stream);
+
+                var fileStream = new FileStream(job.Path, FileMode.Open);
+                fileStream.CopyTo(stream);
+
+                stream.WriteByte(0);
+                stream.Flush();
+            }
+        }
+
+        private static void CheckResult(NetworkStream stream)
+        {
+            var result = stream.ReadByte();
+            if (result != 0)
+            {
+                throw new ApplicationException($"Unexpected response from server on receive job: {result}");
             }
         }
     }
---
+++ lpr/LPRProgram.cs
@@ -0,0 +1,16 @@
+﻿using common;
+
+namespace lpr
+{
+    class LPRProgram
+    {
+        static void Main(string[] args)
+        {
+            var parser = new CommandLineParser<LPRJob>();
+            var job = parser.ParseCommandLine(args);
+
+            ILPRClient lprClient = new LPRClient();
+            lprClient.PrintFile(job);
+        }
+    }
+}
